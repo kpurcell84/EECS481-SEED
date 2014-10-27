6a13ed445db50e5c939541288eba2806e8ca18c3
Dominic Corona <dccorona@umich.edu>
2014-10-26 21:24:25 -0400

    + added code to load in recently asked questions

--- SEEDForAndroid/app/src/main/java/edu/umich/seedforandroid/patient/fragments/mysepsisnurse/MySepsisNurse_RAQ_Frag.java
+++ SEEDForAndroid/app/src/main/java/edu/umich/seedforandroid/patient/fragments/mysepsisnurse/MySepsisNurse_RAQ_Frag.java
@@ -7,12 +7,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.appspot.umichseed.seed.Seed;
+import com.appspot.umichseed.seed.SeedRequest;
+import com.appspot.umichseed.seed.model.SeedApiMessagesWatsonQuestionPut;
+import com.appspot.umichseed.seed.model.SeedApiMessagesWatsonQuestionsListResponse;
+import com.appspot.umichseed.seed.model.SeedApiMessagesWatsonQuestionsRequest;
+
+import java.io.IOException;
+import java.util.List;
+
 import edu.umich.seedforandroid.R;
+import edu.umich.seedforandroid.api.ApiThread;
+import edu.umich.seedforandroid.api.SeedApi;
 
 public class MySepsisNurse_RAQ_Frag extends Fragment  {
 
+    private static final long NUM_QUESTIONS = 10;
+
+    private ApiThread mApiThread;
+
     public MySepsisNurse_RAQ_Frag() {
 
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mApiThread = new ApiThread();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mApiThread.stop();
     }
 
     @Override
@@ -24,5 +53,55 @@ public class MySepsisNurse_RAQ_Frag extends Fragment  {
         actionBar.setTitle("Recently Asked Questions");
 
         return view;
+    }
+
+    // region show/hide loader
+    private void showLoader() {
+
+    }
+
+    private void hideLoader() {
+
+    }
+    // endregion
+
+    private void getQuestions() {
+
+        try {
+
+            showLoader();
+            Seed api = SeedApi.getUnauthenticatedApi();
+            final SeedRequest request =
+                    api.watsonRecentQuestions()
+                    .get(
+                            new SeedApiMessagesWatsonQuestionsRequest()
+                            .setNumQuestions(NUM_QUESTIONS)
+                    );
+
+            mApiThread.enqueRequest(request, new ApiThread.ApiResultAction() {
+                @Override
+                public void onApiResult(Object result) {
+
+                    if (result != null) {
+
+                        SeedApiMessagesWatsonQuestionsListResponse response
+                                = (SeedApiMessagesWatsonQuestionsListResponse)result;
+
+                        populateListView(response.getQuestions());
+                    }
+                }
+            });
+        }
+        catch (IOException ioe) { /*unexpected*/ hideLoader(); ioe.printStackTrace(); }
+    }
+
+    private void populateListView(List<SeedApiMessagesWatsonQuestionPut> questions) {
+
+        /*==============================
+            Put the data into the listview
+            here
+          ==============================*/
+
+        hideLoader();
     }
 }
