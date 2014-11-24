939fa089d075b5efc1eabb8e2e2f2ec7edbbf8ec
Dominic Corona <dccorona@umich.edu>
2014-11-24 18:07:19 -0500

    + code to load profile info for patient and doctor

--- SEEDForAndroid/app/src/main/java/edu/umich/seedforandroid/doctor/fragments/Doctor_Profile_Frag.java
+++ SEEDForAndroid/app/src/main/java/edu/umich/seedforandroid/doctor/fragments/Doctor_Profile_Frag.java
@@ -2,21 +2,109 @@ package edu.umich.seedforandroid.doctor.fragments;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.appspot.umichseed.seed.Seed;
+import com.appspot.umichseed.seed.SeedRequest;
+import com.appspot.umichseed.seed.model.MessagesDoctorPut;
+import com.appspot.umichseed.seed.model.MessagesPatientPut;
+
+import java.io.IOException;
+
 import edu.umich.seedforandroid.R;
+import edu.umich.seedforandroid.account.GoogleAccountManager;
+import edu.umich.seedforandroid.api.ApiThread;
+import edu.umich.seedforandroid.api.SeedApi;
 
 public class Doctor_Profile_Frag extends Fragment  {
+
+    private static final String TAG = Doctor_Profile_Frag.class.getSimpleName();
+
+    private ApiThread mApiThread;
+    private GoogleAccountManager mAccountManager;
 
     public Doctor_Profile_Frag() {
 
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mApiThread = new ApiThread();
+        mAccountManager = new GoogleAccountManager(getActivity());
+        if (!mAccountManager.tryLogIn()) {
+
+            Log.e(TAG, "FATAL ERROR: Somehow, the user got here without being logged in");
+            notifyUiAuthenticationError();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mApiThread.stop();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)  {
 
         return inflater.inflate(R.layout.fragment_doctor__profile_, container, false);
+    }
+
+    private void displayProfileInformation(MessagesDoctorPut doctorProfile) {
+
+        //todo display the profile info here
+    }
+
+    private void notifyUiAuthenticationError() {
+
+        //todo somehow, the user isn't logged in. Alert them and redirect to MainActivity
+    }
+
+    private void notifyUiApiError() {
+
+        //todo there was an API error. Notify the user
+    }
+
+    //todo decide where to call this...probably in onStart?
+    private void loadProfileInformation() {
+
+        try {
+
+            Seed api = SeedApi.getAuthenticatedApi(mAccountManager.getCredential());
+            SeedRequest request = api.doctor().get(mAccountManager.getAccountName());
+            mApiThread.enqueueRequest(request, new ApiThread.ApiResultAction() {
+                @Override
+                public void onApiResult(Object result) {
+
+                    if (result != null && result instanceof MessagesDoctorPut) {
+
+                        displayProfileInformation((MessagesDoctorPut)result);
+                    }
+                    else {
+
+                        Log.e(TAG, "API Error: API returned successfully, but with an invalid datatype (or null)");
+                        notifyUiApiError();
+                    }
+                }
+
+                @Override
+                public void onApiError(Throwable error) {
+
+                    Log.e(TAG, "API Error: API returned failure with messsge " + error.getMessage());
+                    notifyUiApiError();
+                }
+            });
+        }
+        catch (IOException e) {
+
+            Log.e(TAG, "An API Error Occurred: The API couldn't instantiate the request");
+            notifyUiApiError();
+        }
     }
 }
--- SEEDForAndroid/app/src/main/java/edu/umich/seedforandroid/patient/fragments/Patient_Profile_Frag.java
+++ SEEDForAndroid/app/src/main/java/edu/umich/seedforandroid/patient/fragments/Patient_Profile_Frag.java
@@ -4,17 +4,35 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.appspot.umichseed.seed.Seed;
+import com.appspot.umichseed.seed.SeedRequest;
+import com.appspot.umichseed.seed.model.MessagesPatientPut;
+
+import java.io.IOException;
+
 import edu.umich.seedforandroid.R;
+import edu.umich.seedforandroid.account.GoogleAccountManager;
+import edu.umich.seedforandroid.api.ApiThread;
+import edu.umich.seedforandroid.api.SeedApi;
 import edu.umich.seedforandroid.patient.daily_survey.DailySurvey;
 
 public class Patient_Profile_Frag extends Fragment implements View.OnClickListener  {
 
+    private static final String TAG = Patient_Profile_Frag.class.getSimpleName();
+
+    public static final String ARG_PATIENT_EMAIL = "forPatientEmail";
+
     private Button bUpdateProfile;
+    private ApiThread mApiThread;
+    private GoogleAccountManager mAccountManager;
+    private String mPatientEmail = null;
 
     public Patient_Profile_Frag()  {}
 
@@ -23,6 +41,28 @@ public class Patient_Profile_Frag extends Fragment implements View.OnClickListen
 
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(false);
+        mApiThread = new ApiThread();
+
+        mAccountManager = new GoogleAccountManager(getActivity());
+        if (!mAccountManager.tryLogIn()) {
+
+            Log.e(TAG, "FATAL ERROR: Somehow, the user got here without being logged in.");
+            notifyUiAuthenticationError();
+        }
+    }
+
+    @Override
+    public void setArguments(Bundle args) {
+        super.setArguments(args);
+
+        mPatientEmail = args.getString(ARG_PATIENT_EMAIL);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mApiThread.stop();
     }
 
     @Override
@@ -59,5 +99,59 @@ public class Patient_Profile_Frag extends Fragment implements View.OnClickListen
 
         Intent i = new Intent(getActivity(), DailySurvey.class);
         startActivity(i);
+    }
+
+    private void displayProfileInformation(MessagesPatientPut patientProfile) {
+
+        //todo display the profile info here
+    }
+
+    private void notifyUiAuthenticationError() {
+
+        //todo somehow, the user isn't logged in. Alert them and redirect to MainActivity
+    }
+
+    private void notifyUiApiError() {
+
+        //todo there was an API error. Notify the user
+    }
+
+    //todo decide where to call this...probably in onStart?
+    private void loadProfileInformation() {
+
+        try {
+
+            String patientEmail = mPatientEmail != null ?
+                    mPatientEmail : mAccountManager.getAccountName();
+            Seed api = SeedApi.getAuthenticatedApi(mAccountManager.getCredential());
+            SeedRequest request = api.patient().get(patientEmail);
+            mApiThread.enqueueRequest(request, new ApiThread.ApiResultAction() {
+                @Override
+                public void onApiResult(Object result) {
+
+                    if (result != null && result instanceof MessagesPatientPut) {
+
+                        displayProfileInformation((MessagesPatientPut)result);
+                    }
+                    else {
+
+                        Log.e(TAG, "API Error: API returned successfully, but with an invalid datatype (or null)");
+                        notifyUiApiError();
+                    }
+                }
+
+                @Override
+                public void onApiError(Throwable error) {
+
+                    Log.e(TAG, "API Error: API returned failure with messsge " + error.getMessage());
+                    notifyUiApiError();
+                }
+            });
+        }
+        catch (IOException e) {
+
+            Log.e(TAG, "An API Error Occurred: The API couldn't instantiate the request");
+            notifyUiApiError();
+        }
     }
 }
